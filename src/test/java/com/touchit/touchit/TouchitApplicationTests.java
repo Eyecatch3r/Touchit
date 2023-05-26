b1578a103f05c4f6f51e2df519ebf0b580adf693
Aldo Costa <aldo.costa@web.de>
2023-05-26 14:46:33 +0200

    -Init project

---
+++ src/test/java/com/touchit/touchit/TouchitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.touchit.touchit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouchitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
